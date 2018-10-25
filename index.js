bba609a8d0c33025cab8d3c263c07978f34d6adc
Clayton Pabst <claytonpabst@gmail.com>
2018-10-25 02:19:14 -0600

    massive added

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ const serverless = require('serverless-http');
 const express = require('express')
 const app = express()
 
+massive(config.connection).then( db => {      // Returns database. See Massive docs for connection options
+  app.set('db', db)
+  console.log('db')
+}).catch( err => {
+  console.log(err)                          //If Dual Session can't connect to db
+})
+
 app.get('/so', function (req, res) {
   res.send('Hello World!')
 })
